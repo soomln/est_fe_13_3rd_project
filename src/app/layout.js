c04c54e66625e692fda44e8911ec10db91a4d000
s0-p <b00v0429@gmail.com>
2026-08-03 05:36:50 +0900

    refactor: 불필요한 코드 및 파일 삭제

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from 'next/font/google';
 import './reset.sass';
 import './globals.sass';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata = {
   title: 'CallBack',
@@ -19,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='ko' className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang='ko'>
       <head>
         <link
           rel='stylesheet'
