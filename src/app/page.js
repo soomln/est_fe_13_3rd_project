0ede6d7c88068a48207e9a1482d902def4df256c
최수민 <qnfehr948@gmail.com>
2026-08-02 16:58:45 +0900

    feat: Header 컴포넌트 반응형 스타일 및 레이아웃 완성

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,17 @@
-import './page.module.sass';
+// src/app/page.js
+import Header from './_components/common/Header';
 
 export default function Home() {
-  return <>Hello, Next.js!</>;
+  return (
+    <div>
+      {/* 상단 헤더 컴포넌트 */}
+      <Header />
+
+      {/* 메인 콘텐츠 영역 (테스트용) */}
+      <main className='container'>
+        <h1 style={{ marginTop: '40px' }}>메인 페이지 테스트</h1>
+        <p>반응형 레이아웃과 Header 컴포넌트가 제대로 작동하는지 확인합니다.</p>
+      </main>
+    </div>
+  );
 }
