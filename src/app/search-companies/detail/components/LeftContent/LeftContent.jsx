5c82a700b89f5b55a4856051886a5a9d4b9791ba
Polao63 <wndbsrider7@naver.com>
2026-08-04 16:25:49 +0900

    라이브러리 적용 전 commit

---
+++ src/app/search-companies/detail/components/LeftContent/LeftContent.jsx
@@ -0,0 +1,21 @@
+export default function LeftContent(){
+  return(
+    <>
+      <section>
+          <h2>기업 소개</h2>
+      </section>
+
+      <section>
+          <h2>핵심 가치</h2>
+      </section>
+
+      <section>
+          <h2>주요 서비스</h2>
+      </section>
+
+      <section>
+          <h2>복지 및 혜택</h2>
+      </section>
+    </>
+  );
+}
