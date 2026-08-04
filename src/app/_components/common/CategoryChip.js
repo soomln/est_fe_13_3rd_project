64164e4600fec37a834c3b3ede0b222bd9e0f8d0
최수민 <qnfehr948@gmail.com>
2026-08-04 17:46:18 +0900

    fix: git merge 충돌 해결 및 _variables.sass 경로 적용

--- src/app/_components/common/CategoryChip.js
+++ src/app/_components/common/CategoryChip.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import styles from './CategoryChip.module.sass';
+import s from './CategoryChip.module.sass';
 
-export default function CategoryChip({ label, isSelected = false, onClick }) {
+export default function CategoryChip({ text = 'web', isSelected = false, onClick }) {
   return (
-    <button
-      type='button'
-      className={`${styles.category_chip} ${isSelected ? styles.is_selected : ''}`}
-      onClick={onClick}
-    >
-      <span className={`${styles.label_text} ${isSelected ? 'font_body_m_b' : 'font_body_m_r'}`}>{label}</span>
-      <div className={styles.active_line} />
+    <button type='button' className={`${s.category_chip} ${isSelected ? s.selected : ''}`} onClick={onClick}>
+      {text}
     </button>
   );
 }
