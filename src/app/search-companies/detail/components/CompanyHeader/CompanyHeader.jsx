7fb7d7514e37a56f63f6763eccbbf4177cb299f0
Polao63 <wndbsrider7@naver.com>
2026-08-04 16:40:01 +0900

    feat: add CompanyHeader and SummaryCard components; import TabNavigation

--- src/app/search-companies/detail/components/CompanyHeader/CompanyHeader.jsx
+++ src/app/search-companies/detail/components/CompanyHeader/CompanyHeader.jsx
@@ -1,3 +1,13 @@
+import SummaryCard from "../SummaryCard/SummaryCard";
+
 export default function CompanyHeader(){
+  return(
+    <>
+      <SummaryCard/>
+      <div>왼쪽</div>
+
+      <div>오른쪽</div>
+    </>
     
+  );
 }
