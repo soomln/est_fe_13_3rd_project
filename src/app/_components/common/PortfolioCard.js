045980cd7b485487c81f5fbd3a3f0776e910bca6
s0-p <b00v0429@gmail.com>
2026-08-05 00:44:42 +0900

    feat: 포트폴리오 갤러리 UI 구현

--- src/app/_components/common/PortfolioCard.js
+++ src/app/_components/common/PortfolioCard.js
@@ -1,44 +1,36 @@
 import styles from './PortfolioCard.module.sass';
 import ActionBtn from './ActionBtn';
-export default function PortfolioCard({
-  thumbnailUrl = '',
-  title = '',
-  authorName = '이름',
-  authorAvatar = '',
-  likeCount = 50,
-  bookmarkCount = 50,
-  onClick = () => {},
-}) {
+export default function PortfolioCard({ item, onClick = () => {} }) {
   return (
-    <div className={styles.portfolio_card} onClick={onClick}>
+    <li className={styles.portfolio_card} onClick={onClick}>
       {/* 썸네일 & 제목 영역 */}
       <div className={styles.thumb_box}>
-        {thumbnailUrl ? (
-          <img src={thumbnailUrl} alt={title || '포트폴리오 썸네일'} className={styles.thumb_img} />
+        {item.thumbnailUrl !== '' ? (
+          <img src={item.thumbnailUrl} alt={item.title || '포트폴리오 썸네일'} className={styles.thumb_img} />
         ) : (
           <div className={styles.thumb_dummy} />
         )}
         <div className={styles.img_hover}>
-          <h4 className='font_h4'>{title}</h4>
+          <h4 className='font_h4'>{item.title}</h4>
         </div>{' '}
       </div>
 
       {/* 카드 하단 정보 영역 */}
       <div className={styles.info_box}>
         <div className={styles.author_info}>
-          {authorAvatar ? (
-            <img src={authorAvatar} alt={authorName} className={styles.avatar_img} />
+          {item.authorAvatar ? (
+            <img src={authorAvatar} alt={item.authorName} className={styles.avatar_img} />
           ) : (
             <span className={styles.avatar_dummy} />
           )}
-          <span className={`${styles.author_name} font_body_s_b`}>{authorName}</span>
+          <span className={`${styles.author_name} font_body_s_b`}>{item.authorName}</span>
         </div>
 
-        <div className={styles.stats_info}>
+        <div className={styles.actions}>
           <ActionBtn iconText={'thumb_up_alt'} count={50} />
           <ActionBtn iconText={'bookmark'} count={50} />
         </div>
       </div>
-    </div>
+    </li>
   );
 }
