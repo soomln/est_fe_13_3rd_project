ca6ea86c15a1e9931e2a5d91e986c3503c505164
s0y0ungk <soyo2039@gmail.com>
2026-08-05 23:56:43 +0900

    feat: 인터뷰 컴포넌트 추가

--- src/app/interview/page.js
+++ src/app/interview/page.js
@@ -1,5 +1,47 @@
 import './page.module.sass';
 
-export default function Home() {
-  return <>Hello, Next.js!</>;
+import Header from '../_components/common/Header';
+import Footer from '../_components/common/Footer';
+
+import InterviewStart from './components/InterviewStart';
+import CompanyCard from './components/CompanyCard';
+import GuideCard from './components/GuideCard';
+
+export default function InterviewPage() {
+  return (
+    <>
+      <Header />
+
+      <main className="interview_page">
+        <div className="container">
+
+          {/* 히어로 */}
+          <section className="hero_section">
+          </section>
+
+          {/* 관심기업 + 자주 묻는 질문 */}
+          <section className="content_section">
+
+            <section className="company_section">
+            </section>
+
+            <section className="question_section">
+            </section>
+
+          </section>
+
+          {/* 핵심 기능 */}
+          <section className="feature_section">
+          </section>
+
+          {/* AI 면접 가이드 */}
+          <section className="guide_section">
+          </section>
+
+        </div>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
