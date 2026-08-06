016f75902096746d32ea4dabbff974903602d7bd
s0y0ungk <soyo2039@gmail.com>
2026-08-06 14:07:02 +0900

    feat: 인터뷰 컴포넌트 변경

--- src/app/interview/components/FeedbackButton/index.js
+++ src/app/interview/components/FeedbackButton/index.js
@@ -1,13 +1,1 @@
-import './FeedbackButton.sass';
-
-export default function FeedbackButton({ onClick }) {
-  return (
-    <button
-      type="button"
-      className="feedback_btn font_h4"
-      onClick={onClick}
-    >
-      피드백 확인하기
-    </button>
-  );
-}
+export {difault} from './FeedbackButton';
--- src/app/interview/components/RetryButton/index.js
+++ src/app/interview/components/RetryButton/index.js
@@ -1,13 +1,1 @@
-import './RetryButton.sass';
-
-export default function RetryButton({ onClick }) {
-  return (
-    <button
-      type="button"
-      className="retry_btn font_h4"
-      onClick={onClick}
-    >
-      다시 연습하기
-    </button>
-  );
-}
+export { difault } from './RetryButton';
