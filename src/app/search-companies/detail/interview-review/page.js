748b27a72c5c7d432797da9145da110d7a47e022
Polao63 <wndbsrider7@naver.com>
2026-08-06 17:42:34 +0900

    feat: refactor company data structure and create interview review page

---
+++ src/app/search-companies/detail/interview-review/page.js
@@ -0,0 +1,14 @@
+// import Header from '@/app/_components/common/Header';
+// import Footer from '@/app/_components/common/Footer';
+import company from "../data/company";
+import CompanyHeader from "../components/CompanyHeader/CompanyHeader";
+import TabNavigation from "../components/TabNavigation/TabNavigation";
+
+export default function InterviewReviewPage(){
+  return(
+    <div>
+      <CompanyHeader company={company}/>
+      <TabNavigation/>
+    </div>
+  );
+}
