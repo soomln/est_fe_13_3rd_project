37f54232834eef92db1462e89e45914e6fea2b6a
s0y0ungk <soyo2039@gmail.com>
2026-08-06 14:07:02 +0900

    feat: 인터뷰 컴포넌트 변경

--- src/app/interview/components/ActionButton/index.js
+++ src/app/interview/components/ActionButton/index.js
@@ -1,16 +1,1 @@
-import './ActionButton.sass';
-
-export default function ActionButton({
-  text,
-  onClick,
-}) {
-  return (
-    <button
-      type="button"
-      className="action_button font_body_l_b"
-      onClick={onClick}
-    >
-      {text}
-    </button>
-  );
-}
+export { default } from './ActionButton';
