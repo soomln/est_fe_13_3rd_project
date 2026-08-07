654d9cd31cee37983de48cda3a6bf0229304ad62
Polao63 <wndbsrider7@naver.com>
2026-08-07 10:14:51 +0900

    feat: implement TabNavigation component and integrate it into InterviewReviewPage

--- src/app/search-companies/detail/interview-review/page.js
+++ src/app/search-companies/detail/interview-review/page.js
@@ -1,5 +1,5 @@
-// import Header from '@/app/_components/common/Header';
-// import Footer from '@/app/_components/common/Footer';
+import Header from '@/app/_components/common/Header';
+import Footer from '@/app/_components/common/Footer';
 import company from "../data/company";
 import CompanyHeader from "../components/CompanyHeader/CompanyHeader";
 import TabNavigation from "../components/TabNavigation/TabNavigation";
@@ -7,8 +7,10 @@ import TabNavigation from "../components/TabNavigation/TabNavigation";
 export default function InterviewReviewPage(){
   return(
     <div>
+      <Header/>
       <CompanyHeader company={company}/>
       <TabNavigation/>
+      <Footer/>
     </div>
   );
 }
