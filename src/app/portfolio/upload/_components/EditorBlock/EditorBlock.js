3de980781b7ce36d1e297d6b95caeb1c70badf43
s0-p <b00v0429@gmail.com>
2026-08-10 23:26:23 +0900

    feat: text block 기능 구현

---
+++ src/app/portfolio/upload/_components/EditorBlock/EditorBlock.js
@@ -0,0 +1,25 @@
+import styles from './EditorBlock.module.sass';
+
+import TextBlock from '../TextBlock';
+// import ImageBlock from '../ImageBlock';
+// import VideoBlock from '../VideoBlock';
+// import CodeBlock from '../CodeBlock';
+
+export default function EditorBlock({ block, updateBlock, removeBlock }) {
+  switch (block.type) {
+    case 'text':
+      return <TextBlock block={block} updateBlock={updateBlock} removeBlock={removeBlock} />;
+
+    //   case 'image':
+    //     return <ImageBlock block={block} updateBlock={updateBlock} removeBlock={removeBlock} />;
+
+    //   case 'video':
+    //     return <VideoBlock block={block} updateBlock={updateBlock} removeBlock={removeBlock} />;
+
+    //   case 'code':
+    //     return <CodeBlock block={block} updateBlock={updateBlock} removeBlock={removeBlock} />;
+
+    default:
+      return null;
+  }
+}
