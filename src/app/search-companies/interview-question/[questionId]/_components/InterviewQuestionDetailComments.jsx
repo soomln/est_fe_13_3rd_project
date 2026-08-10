03d1d360177f4dd253137e3c133a749b53e95720
Polao63 <wndbsrider7@naver.com>
2026-08-10 22:30:31 +0900

    refator: 각각 가져올 후기, 질문 데이터를 분리중.

--- src/app/search-companies/interview-question/[questionId]/_components/InterviewQuestionDetailComments.jsx
+++ src/app/search-companies/interview-question/[questionId]/_components/InterviewQuestionDetailComments.jsx
@@ -1,3 +1,25 @@
-export default function InterviewQuestionDetailComments(){
-  
+const sortOptions = ["추천순", "최신순"];
+
+
+export default function InterviewQuestionDetailComments({question}){
+  return(
+    <div>
+      <span>
+        {/* <img src="" alt="프로필 사진" /> */}
+        <span>프로필 사진</span>
+        <input type="text" placeholder="댓글을 입력해주세요." id="" /><button type="button">등록</button>
+      </span>
+      <select>
+        {sortOptions.map((option) => (
+          <option key={option}>{option}</option>
+        ))}
+      </select>
+      {question.comments.map((comment) => (
+      <div key={comment.id}>
+        <p>{comment.job} / {comment.education} {comment.date} {comment.time}</p>
+        <p>{comment.content} 좋아요 수: {comment.likes}</p>
+      </div>
+      ))}
+    </div>
+  );
 }
--- src/app/search-companies/interview-question/[questionId]/_components/InterviewQuestionDetailHeader.jsx
+++ src/app/search-companies/interview-question/[questionId]/_components/InterviewQuestionDetailHeader.jsx
@@ -1,28 +1,28 @@
 import styles from "@/app/search-companies/_components/InterviewReviewCard.module.sass"
 
-export default function InterviewQuestionDetailHeader(){
+export default function InterviewQuestionDetailHeader({question}){
   return(
     <div className={styles.card_header}>
       <div className={styles.meta_line}>
         <div className={styles.meta_tags}>
           <div className={styles.meta_item}>
             <span className={`font_body_m_b ${styles.label}`}>면접 난이도</span>
-            <span className={`font_body_m_r ${styles.value} ${styles.green}`}>{review.difficulty}</span>
+            <span className={`font_body_m_r ${styles.value} ${styles.green}`}>{question.difficulty}</span>
           </div>
           <div className={styles.meta_item}>
             <span className={`font_body_m_b ${styles.label}`}>합격 여부</span>
-            <span className={`font_body_m_r ${styles.value}`}>{review.result}</span>
+            <span className={`font_body_m_r ${styles.value}`}>{question.result}</span>
           </div>
           <div className={styles.meta_item}>
             <span className={`font_body_m_b ${styles.label}`}>면접 경로</span>
-            <span className={`font_body_m_r ${styles.value}`}>{review.route}</span>
+            <span className={`font_body_m_r ${styles.value}`}>{question.route}</span>
           </div>
         </div>
 
         <div className={styles.writer_info}>
           <span className={`material-symbols-rounded ${styles.writer_icon}`}>account_circle</span>
-          <span className={`font_body_m_r ${styles.job_text}`}>{review.job} / {review.education}</span>
-          <span className={`font_body_m_r ${styles.date_text}`}>{review.date}</span>
+          <span className={`font_body_m_r ${styles.job_text}`}>{question.job} / {question.education}</span>
+          <span className={`font_body_m_r ${styles.date_text}`}>{question.date}</span>
         </div>
       </div>
     </div>
