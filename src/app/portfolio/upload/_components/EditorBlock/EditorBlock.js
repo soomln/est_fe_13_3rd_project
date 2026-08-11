3ee919eb5fb0b5b304919d7917b25eb9bf4b873d
s0-p <b00v0429@gmail.com>
2026-08-11 13:28:17 +0900

    feat: video block 추가 기능 구현

--- src/app/portfolio/upload/_components/EditorBlock/EditorBlock.js
+++ src/app/portfolio/upload/_components/EditorBlock/EditorBlock.js
@@ -2,7 +2,7 @@ import styles from './EditorBlock.module.sass';
 
 import TextBlock from '../TextBlock';
 import ImageBlock from '../ImageBlock';
-// import VideoBlock from '../VideoBlock';
+import VideoBlock from '../VideoBlock';
 // import CodeBlock from '../CodeBlock';
 
 export default function EditorBlock({ block, updateBlock, removeBlock }) {
@@ -13,11 +13,11 @@ export default function EditorBlock({ block, updateBlock, removeBlock }) {
     case 'image':
       return <ImageBlock block={block} updateBlock={updateBlock} removeBlock={removeBlock} />;
 
-    //   case 'video':
-    //     return <VideoBlock block={block} updateBlock={updateBlock} removeBlock={removeBlock} />;
+    case 'video':
+      return <VideoBlock block={block} updateBlock={updateBlock} removeBlock={removeBlock} />;
 
-    //   case 'code':
-    //     return <CodeBlock block={block} updateBlock={updateBlock} removeBlock={removeBlock} />;
+    // case 'code':
+    //   return <CodeBlock block={block} updateBlock={updateBlock} removeBlock={removeBlock} />;
 
     default:
       return null;
