af3f513cad8e6a1c6174a4b89fa0d29c04357f79
s0-p <b00v0429@gmail.com>
2026-08-11 16:06:32 +0900

    fix: review card 수정

--- src/app/_components/common/ReviewCard/ReviewCard.js
+++ src/app/_components/common/ReviewCard/ReviewCard.js
@@ -7,55 +7,50 @@ import styles from './ReviewCard.module.sass';
 export default function ReviewCard({
   companyLogo = null,
   companyName = '',
-  difficulty,
-  result,
-  channel,
-  jobInfo,
-  date,
-  questions,
-  saveCount,
-  commentCount,
+  review,
   onBookmarkClick, // 백엔드 구현 시 수정 필요
   onClick,
 }) {
   return (
-    <div className={styles.review_card} onClick={onClick}>
+    <li key={review.id} className={styles.review_card} onClick={onClick}>
       <div className={styles.bookmark_wrapper}>
         <Bookmark size='large' onClick={onBookmarkClick} />
       </div>
 
       <div className={styles.card_header}>
-        <div className={styles.company_info}>
-          {companyLogo && <img src={companyLogo} alt={`${companyName} 로고`} className={styles.logo_img} />}
-          {companyName && <span className={`font_body_m_b ${styles.company_name}`}>{companyName}</span>}
-        </div>
+        {companyLogo && (
+          <div className={styles.company_info}>
+            <img src={companyLogo} alt={`${companyName} 로고`} className={styles.logo_img} />
+            {companyName && <span className={`font_body_m_b ${styles.company_name}`}>{companyName}</span>}
+          </div>
+        )}
 
         <div className={styles.meta_line}>
           <div className={styles.meta_tags}>
             <div className={styles.meta_item}>
               <span className={`font_body_m_b ${styles.label}`}>면접 난이도</span>
-              <span className={`font_body_m_r ${styles.value} ${styles.green}`}>{difficulty}</span>
+              <span className={`font_body_m_r ${styles.value} ${styles.green}`}>{review.difficulty}</span>
             </div>
             <div className={styles.meta_item}>
               <span className={`font_body_m_b ${styles.label}`}>합격 여부</span>
-              <span className={`font_body_m_r ${styles.value}`}>{result}</span>
+              <span className={`font_body_m_r ${styles.value}`}>{review.result}</span>
             </div>
             <div className={styles.meta_item}>
               <span className={`font_body_m_b ${styles.label}`}>면접 경로</span>
-              <span className={`font_body_m_r ${styles.value}`}>{channel}</span>
+              <span className={`font_body_m_r ${styles.value}`}>{review.route}</span>
             </div>
           </div>
 
           <div className={styles.writer_info}>
             <span className={`material-symbols-rounded ${styles.writer_icon}`}>account_circle</span>
-            <span className={`font_body_m_r ${styles.job_text}`}>{jobInfo}</span>
-            <span className={`font_body_m_r ${styles.date_text}`}>{date}</span>
+            <span className={`font_body_m_r ${styles.job_text}`}>{`${review.job} / ${review.education}`}</span>
+            <span className={`font_body_m_r ${styles.date_text}`}>{review.date}</span>
           </div>
         </div>
       </div>
 
       <div className={styles.question_list}>
-        {questions.map((q, idx) => (
+        {review.questions.map((q, idx) => (
           <p key={idx} className={`font_body_m_r ${styles.question_item}`}>
             {q}
           </p>
@@ -66,12 +61,12 @@ export default function ReviewCard({
         <div className={styles.save_box}>
           <span className={`material-symbols-sharp ${styles.fill_icon}`}>bookmark</span>
           <span>퍼가요</span>
-          <span>{saveCount}</span>
+          <span>{review.bookmark}</span>
         </div>
         <div className={styles.comment_box}>
-          <span>댓글 {commentCount}</span>
+          <span>댓글 {review.comment}</span>
         </div>
       </div>
-    </div>
+    </li>
   );
 }
