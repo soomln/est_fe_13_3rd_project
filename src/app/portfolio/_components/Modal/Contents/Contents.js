ff531cbe43548912206c1f72d9588ef14a821dd5
s0-p <b00v0429@gmail.com>
2026-08-13 03:02:46 +0900

    feat: db 연결 및 Text Block 수정

--- src/app/portfolio/_components/Modal/Contents/Contents.js
+++ src/app/portfolio/_components/Modal/Contents/Contents.js
@@ -1,20 +1,20 @@
 import styles from './Contents.module.sass';
 
-import EditorBlock from '@/app/portfolio/upload/_components/EditorBlock';
+import BlockManager from '@/app/portfolio/upload/_components/BlockManager';
 
-export default function Contents({
-  bgColor = '#ffffff',
-  gap = '1rem',
-  contentsRef = null,
-  blocks = [],
-  updateBlock,
-  removeBlock,
-}) {
+export default function Contents({ item, contentsRef = null, isEditMode = false, updateBlock, removeBlock }) {
   return (
-    <div ref={contentsRef} className={`${styles.container}`} style={{ backgroundColor: bgColor }}>
-      <div className={`${styles.contents}`} style={{ gap: `${gap}px` }}>
-        {blocks.map((block) => (
-          <EditorBlock key={block.id} block={block} updateBlock={updateBlock} removeBlock={removeBlock} />
+    <div ref={contentsRef} className={styles.container} style={{ backgroundColor: item?.bgColor }}>
+      <div className={styles.contents} style={{ gap: `${item?.gap}px` }}>
+        {item?.content?.map((block, idx) => (
+          <BlockManager
+            key={block.id ?? idx}
+            block={block}
+            index={idx}
+            isEditMode={isEditMode}
+            updateBlock={updateBlock}
+            removeBlock={removeBlock}
+          />
         ))}
       </div>
     </div>
