34cfdbcd8ccd2478ff9561505b6c61d12a44bbfc
s0-p <b00v0429@gmail.com>
2026-08-13 03:40:42 +0900

    feat: content 데이터 구조 변경에 따른 수정

--- src/app/portfolio/upload/page.js
+++ src/app/portfolio/upload/page.js
@@ -7,7 +7,6 @@ import TabGroup from '../_components/Modal/TabGroup';
 import Contents from '../_components/Modal/Contents';
 import UploadBtn from './_components/UploadBtn';
 import CustomSetting from './_components/CustomSetting';
-import BlockManager from './_components/BlockManager';
 import AiChatPanel from './_components/AiChatPanel';
 import AiChatBtn from './_components/AiChatBtn';
 import BackBtn from './_components/BackBtn';
@@ -20,46 +19,47 @@ export default function Upload() {
     gap: 16,
     content: [],
   });
+
   const [activeTab, setActiveTab] = useState('ai');
   const [isChatOpen, setIsChatOpen] = useState(false);
   const [messages, setMessages] = useState([]);
 
-  const addBlock = (_type) => {
+  const addBlock = (type) => {
     const newBlock = {
       id: crypto.randomUUID(),
-      type: _type,
+      type,
     };
 
-    if (_type === 'text') {
+    if (type === 'text') {
       newBlock.html = '';
     }
 
-    if (_type === 'image') {
-      // newBlock.file = null;
+    if (type === 'image') {
       newBlock.url = '';
     }
 
-    if (_type === 'video') {
+    if (type === 'video') {
       newBlock.url = '';
     }
 
-    if (_type === 'code') {
+    if (type === 'code') {
       newBlock.code = '';
     }
 
-    setItem((prev) => ({ ...prev, content: [...prev.content, newBlock] }));
-
-    console.log(_type, newBlock);
-  };
-
-  const updateBlock = (index, newData) => {
     setItem((prev) => ({
       ...prev,
-      content: prev.content.map((block, idx) =>
-        idx === index
+      content: [...prev.content, newBlock],
+    }));
+  };
+
+  const updateBlock = (id, updatedBlock) => {
+    setItem((prev) => ({
+      ...prev,
+      content: prev.content.map((block) =>
+        block.id === id
           ? {
               ...block,
-              ...newData,
+              ...updatedBlock,
             }
           : block,
       ),
@@ -67,7 +67,10 @@ export default function Upload() {
   };
 
   const removeBlock = (id) => {
-    setContent((prev) => prev.filter((block) => block.id !== id));
+    setItem((prev) => ({
+      ...prev,
+      content: prev.content.filter((block) => block.id !== id),
+    }));
   };
 
   const setBgColor = (bgColor) => {
@@ -92,7 +95,8 @@ export default function Upload() {
             <AiChatPanel onClose={() => setIsChatOpen(false)} messages={messages} setMessages={setMessages} />
           </aside>
         )}
-        <div className={`${styles.workspace}`}>
+
+        <div className={styles.workspace}>
           <header>
             <div className={styles.btns}>
               {!isChatOpen && (
@@ -102,26 +106,38 @@ export default function Upload() {
                   }}
                 />
               )}
+
               <BackBtn />
             </div>
+
             <div className={styles.btns}>
               <SaveBtn iconText='save' text='임시 저장' textColor='#00A63D' bgColor='#EEFDF3' />
+
               <SaveBtn iconText='upload' text='업로드' textColor='#FFFFFF' bgColor='#111111' />
             </div>
           </header>
+
           <main>
             <TabGroup bgColor={item.bgColor} activeTab={activeTab} onChangeTab={setActiveTab} />
+
             <Contents item={item} isEditMode={true} updateBlock={updateBlock} removeBlock={removeBlock} />
           </main>
+
           <aside className={styles.btns_wrapper}>
             <div className={styles.add_btns}>
               <UploadBtn iconText='image' text='이미지' isIconFill={true} onClick={() => addBlock('image')} />
+
               <UploadBtn iconText='ondemand_video' text='동영상' onClick={() => addBlock('video')} />
+
               <UploadBtn iconText='text_fields' text='텍스트' onClick={() => addBlock('text')} />
+
               <UploadBtn iconText='code' text='코드' onClick={() => addBlock('code')} />
             </div>
+
             <UploadBtn iconText='import_export' text='순서 바꾸기' />
+
             <CustomSetting bgColor={item.bgColor} onSetColor={setBgColor} gap={item.gap} onSetGap={setGap} />
+
             <div className={styles.save_btns}></div>
           </aside>
         </div>
