bbe95ac3f18e42e15a699fb3c794dd81f9c6643c
s0-p <b00v0429@gmail.com>
2026-08-14 21:21:40 +0900

    feat: 카테고리 필터 기능 구현

--- src/app/portfolio/_components/CategoryBtn/CategoryBtn.js
+++ src/app/portfolio/_components/CategoryBtn/CategoryBtn.js
@@ -1,8 +1,8 @@
 import styles from './CategoryBtn.module.sass';
 
-export default function CategoryBtn({ category = '', initChecked = false }) {
+export default function CategoryBtn({ category = '', initChecked = false, onClick }) {
   return (
-    <label className={`${styles.category_btn} font_body_m_b`}>
+    <label className={`${styles.category_btn} font_body_m_b`} onClick={onClick}>
       <input type='radio' name='category' value={category} defaultChecked={initChecked} />
       {category}
     </label>
