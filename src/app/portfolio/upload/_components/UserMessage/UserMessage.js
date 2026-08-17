23ec7382dbb0a26ce02133c8c034938af59921dc
s0-p <b00v0429@gmail.com>
2026-08-18 06:44:39 +0900

    feat: ai 채팅 스타일 변경

--- src/app/portfolio/upload/_components/UserMessage/UserMessage.js
+++ src/app/portfolio/upload/_components/UserMessage/UserMessage.js
@@ -1,5 +1,5 @@
 import styles from './UserMessage.module.sass';
 
 export default function UserMessage({ message }) {
-  return <li className={styles.bubble}>{message}</li>;
+  return <li className={`${styles.bubble} font_body_m_s`}>{message}</li>;
 }
