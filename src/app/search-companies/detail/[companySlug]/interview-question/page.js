b56ffec7f606d88de0076e536ec0f1808ab56795
Polao63 <wndbsrider7@naver.com>
2026-08-20 17:56:58 +0900

    메타데이터 추가.

--- src/app/search-companies/detail/[companySlug]/interview-question/page.js
+++ src/app/search-companies/detail/[companySlug]/interview-question/page.js
@@ -1,5 +1,16 @@
 import InterviewQuestionReviewClient from './_components/InterviewQuestionReviewClient';
 
+export const metadata = {
+  title: '면접 족보 | CallBack',
+  description: '기업별 실제 면접 질문과 면접 족보를 확인해보세요.',
+
+    openGraph: {
+    title: '면접 족보 | CallBack',
+    description: '기업별 실제 면접 질문과 면접 족보를 확인해보세요.',
+    type: 'website',
+  },
+};
+
 export default async function InterviewQuestionReviewPage({ params }) {
   const { companySlug } = await params;
 
