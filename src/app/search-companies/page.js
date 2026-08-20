156dc7e6f14ea7f27997579d8fec3c93337fc027
Polao63 <wndbsrider7@naver.com>
2026-08-20 18:38:13 +0900

    feat: OpenGraph 이미지 추가.

--- src/app/search-companies/page.js
+++ src/app/search-companies/page.js
@@ -2,10 +2,18 @@ import { Suspense } from 'react';
 
 import SearchCompaniesClient from './_components/SearchCompaniesClient';
 
- export const metadata = {
+export const metadata = {
   title: 'CallBack',
   description: '개발자를 위한 취업 준비 플랫폼. 당신의 취업 준비를 최적화 하세요!',
+
+  openGraph: {
+    title: 'CallBack',
+    description: '개발자를 위한 취업 준비 플랫폼. 당신의 취업 준비를 최적화 하세요!',
+    type: 'website',
+    images: ['/public/images/OG_Image.png'],
+  },
 };
+
 
 export default function SearchCompaniesPage() {
   return (
