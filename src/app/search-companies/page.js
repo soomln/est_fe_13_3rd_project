75d838a082690aaee35c3e31ed88aab605a635fe
s0-p <b00v0429@gmail.com>
2026-08-20 21:32:51 +0900

    chore: 메타데이터 title 수정

--- src/app/search-companies/page.js
+++ src/app/search-companies/page.js
@@ -3,11 +3,11 @@ import { Suspense } from 'react';
 import SearchCompaniesClient from './_components/SearchCompaniesClient';
 
 export const metadata = {
-  title: 'CallBack | 기업 탐색',
+  title: '기업 탐색 | CallBack',
   description: '기업 정보부터 면접 후기, 족보까지 확인하고, 나에게 맞는 기업을 찾아보세요!',
 
   openGraph: {
-    title: 'CallBack | 기업 탐색',
+    title: '기업 탐색 | CallBack',
     description: '기업 정보부터 면접 후기, 족보까지 확인하고, 나에게 맞는 기업을 찾아보세요!',
     type: 'website',
   },
