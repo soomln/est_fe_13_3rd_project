63a4dba5c5f5728795c85f2351c763d666aa7c0f
s0-p <b00v0429@gmail.com>
2026-08-20 19:41:16 +0900

    feat: 메타데이터 추가

--- src/app/search-companies/page.js
+++ src/app/search-companies/page.js
@@ -3,17 +3,15 @@ import { Suspense } from 'react';
 import SearchCompaniesClient from './_components/SearchCompaniesClient';
 
 export const metadata = {
-  title: 'CallBack',
-  description: '개발자를 위한 취업 준비 플랫폼. 당신의 취업 준비를 최적화 하세요!',
+  title: 'CallBack | 기업 탐색',
+  description: '기업 정보부터 면접 후기, 족보까지 확인하고, 나에게 맞는 기업을 찾아보세요!',
 
   openGraph: {
-    title: 'CallBack',
-    description: '개발자를 위한 취업 준비 플랫폼. 당신의 취업 준비를 최적화 하세요!',
+    title: 'CallBack | 기업 탐색',
+    description: '기업 정보부터 면접 후기, 족보까지 확인하고, 나에게 맞는 기업을 찾아보세요!',
     type: 'website',
-    images: ['/images/OG_Image.png'],
   },
 };
-
 
 export default function SearchCompaniesPage() {
   return (
